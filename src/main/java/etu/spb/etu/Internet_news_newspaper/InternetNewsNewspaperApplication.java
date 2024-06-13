98fefb46d88ff7bd810aa719dc30506ca91755ea
Бородулин Максим Дмитриевич <dborodulin@nic.etu>
2024-06-13 16:43:38 +0300

    init repository

---
+++ src/main/java/etu/spb/etu/Internet_news_newspaper/InternetNewsNewspaperApplication.java
@@ -0,0 +1,13 @@
+package etu.spb.etu.Internet_news_newspaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetNewsNewspaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetNewsNewspaperApplication.class, args);
+	}
+
+}
